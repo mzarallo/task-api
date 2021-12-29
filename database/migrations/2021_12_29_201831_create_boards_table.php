d068c64a5bcc85e25862a4903bb2a503db46d64c
Mauricio Zarallo <mauricio.zarallo@gmail.com>
2021-12-29 17:52:49 -0300

    feat(migrations): añade migraciones

---
+++ database/migrations/2021_12_29_201831_create_boards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->unsignedBigInteger('author_id')->nullable(false);
+            $table->string('hex_color')->nullable(false);
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
