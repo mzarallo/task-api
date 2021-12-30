86f096f0bfd0520df8fa15740825dd693b571149
Mauricio Zarallo <mauricio.zarallo@gmail.com>
2021-12-30 14:55:25 -0300

    test(feature): añade test endpoint login

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function user_can_obtain_jwt_token_with_correct_credentials(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->json('POST', route('api.authentication.login'), [
+            'email' => User::all()->random()->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertJson(fn (AssertableJson $json) => $json
+                ->hasAll('token_type', 'access_token', 'expires_in')
+                ->where('token_type', 'bearer')
+                ->whereAllType([
+                    'token_type' => 'string',
+                    'expires_in' => 'integer',
+                    'access_token' => 'string',
+                ])
+                ->etc()
+        )->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function user_cant_obtain_jwt_token_with_incorrect_credentials(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->json('POST', route('api.authentication.login'), [
+            'email' => 'fakemail@email.com',
+            'password' => 'password',
+        ]);
+
+        $response
+            ->assertJson(['message' => 'Las credenciales no coinciden nuestros registros'])
+            ->assertStatus(401);
+    }
+}
