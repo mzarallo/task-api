8b0e403fb3a0a8c4b499f44e05c13a6a4627f269
Mauricio Zarallo <mauricio.zarallo@gmail.com>
2022-01-01 13:36:40 -0300

    style(php-fixer): aplica formateo de codigo php-cs-fixer

--- tests/Feature/PermissionTest.php
+++ tests/Feature/PermissionTest.php
@@ -10,28 +10,32 @@ use Tests\TestCase;
 
 class PermissionTest extends TestCase
 {
-    public function test_user_can_obtain_all_permissions()
+    /**
+     * @test
+     */
+    public function user_can_obtain_all_permissions(): void
     {
         $this->actingAs(User::all()->first());
 
         $response = $this->json('GET', route('api.permissions.all'));
 
-        $response->assertJson(fn (AssertableJson $json) =>
-            $json->has('data', 21)->has('data.0', fn (AssertableJson $json) =>
-                $json->hasAll('id', 'name', 'category', 'guard', 'created_at', 'updated_at')
+        $response->assertJson(fn (AssertableJson $json) => $json->has('data.0', fn (AssertableJson $json) => $json->hasAll('id', 'name', 'category', 'guard', 'created_at', 'updated_at')
                     ->whereAllType([
                         'id' => 'integer',
                         'name' => 'string',
                         'category' => 'string',
                         'guard' => 'string',
                         'created_at' => 'string',
-                        'updated_at' => 'string'
+                        'updated_at' => 'string',
                     ])
             )
         )->assertStatus(200);
     }
 
-    public function test_user_cannot_get_permissions_without_authorization()
+    /**
+     * @test
+     */
+    public function user_cannot_get_permissions_without_authorization(): void
     {
         $this->actingAs(User::find(2));
 
