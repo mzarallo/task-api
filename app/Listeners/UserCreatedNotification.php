8381d3f4c8bc994cb605f4c3466b63cebe2ddf37
Mauricio Zarallo <mauricio.zarallo@gmail.com>
2022-01-05 15:47:05 -0300

    feat(mail): añade envío de email con credenciales al usuario
    Añade listener para esuchar evento de creación de usuario y enviar email con las credenciales.

---
+++ app/Listeners/UserCreatedNotification.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Listeners;
+
+use App\Actions\User\SendCredentialsEmailToUser;
+use App\Events\UserCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserCreatedNotification implements ShouldQueue
+{
+    public $queue = 'listeners';
+
+    public function __construct(private SendCredentialsEmailToUser $emailWithCredentialToUser)
+    {
+    }
+
+    public function handle(UserCreated $event): void
+    {
+        $this->emailWithCredentialToUser->run($event->user, $event->password);
+    }
+}
